f9d35d3020f884337d0c6d67eb954ea9188949a5
jawad727 <jawaddhussein@gmail.com>
2019-04-15 14:25:29 -0700

    MVP Complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,17 +5,29 @@ module.exports = {
   get,
 };
 
+
+
 function succeed(item) {
-  return { ...item };
+  const num = item.enhancement
+
+  if (num < 20) {return { ...item, enhancement: num + 1}};
 }
+
 
 function fail(item) {
-  return { ...item };
+  const num = item.enhancement
+  const dur = item.durability
+
+  if (num < 15) {return { ...item, durability: dur - 5}}
+  else if (num >= 15) {return { ...item, durability: dur - 10}}
+  else if (num > 16) {return { ...item, enhancement: num - 1}}
 }
 
+
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100, };
 }
+
 
 function get(item) {
   return { ...item };
